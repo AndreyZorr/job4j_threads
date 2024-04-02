fb1a741b45664f578405d04987347922f3ea9a0c
Andrey <AlpatoV_A_A@mail.ru>
2024-04-02 22:23:45 +0300

    8. Скачивание файла с ограничением. [#144271]

--- src/ru/job4j/concurrent/Wget.java
+++ src/ru/job4j/concurrent/Wget.java
@@ -1,20 +1,61 @@
 package ru.job4j.concurrent;
 
-public class Wget {
-    public static void main(String[] args) {
-        Thread thread = new Thread(
-                () -> {
-                    for (int index = 0; index < 100; index++) {
-                        try {
-                            System.out.print("\rLoading : " + index + "%");
-                            Thread.sleep(1000);
-                        } catch (InterruptedException e) {
-                            throw new RuntimeException(e);
-                        }
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+public class Wget implements Runnable {
+    private final String url;
+    private final int speed;
+
+    private final String file;
+    private static final int SECOND = 1000;
+
+    public Wget(String url, int speed, String file) {
+        this.url = url;
+        this.speed = speed;
+        this.file = file;
+    }
+
+    @Override
+    public void run() {
+        try (InputStream in = new URL(url).openStream();
+             FileOutputStream out = new FileOutputStream(file)) {
+            long start = System.currentTimeMillis();
+            int totalBytesRead = 0;
+            int bytesRead;
+
+            while ((bytesRead = in.read()) != -1) {
+                out.write(bytesRead);
+                totalBytesRead += bytesRead;
+
+                if (totalBytesRead >= speed) {
+                    long time = System.currentTimeMillis() - start;
+                    if (time < SECOND) {
+                        long sleepTime = SECOND - time;
+                        Thread.sleep(sleepTime);
+                        System.out.println("sleep for: " + sleepTime);
+                        System.out.println("readBytes: " + totalBytesRead);
                     }
+                    start = System.currentTimeMillis();
+                    totalBytesRead = 0;
                 }
-        );
-        thread.start();
-        System.out.println();
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        if (args.length < 2) {
+            throw new IllegalArgumentException("Usage: Wget <URL> <speed>");
+        }
+        String url = args[0];
+        int speed = Integer.parseInt(args[1]);
+        String file = args[2];
+        Thread wget = new Thread(new Wget(url, speed, file));
+        wget.start();
+        wget.join();
     }
 }
