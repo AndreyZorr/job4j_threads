0635c354b84d237a26473b3cdbf69bc7d52197f2
Andrey <AlpatoV_A_A@mail.ru>
2024-04-17 22:50:29 +0300

    5. Состояние гонки [#504909]

---
+++ src/ru/job4j/threads/threadlocal/Singleton.java
@@ -0,0 +1,15 @@
+package ru.job4j.threads.threadlocal;
+
+public class Singleton {
+    private static Singleton instance;
+
+    private Singleton() {
+    }
+
+    public static Singleton getInstance() {
+        if (instance == null) {
+            instance = new Singleton();
+        }
+        return instance;
+    }
+}
